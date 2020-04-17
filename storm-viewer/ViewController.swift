635d3a69f660efe3a0505fa39f3ff7dc7215a5a7
Vinh Le <vinh@vertics.co>
2020-04-17 06:32:27 +0300

    feat: init project with image name table view

--- storm-viewer/ViewController.swift
+++ storm-viewer/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private var pictures = [String]()
+    private let cellId = "pictureCell"
+    var tableView:UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        renderTableView()
+        loadImages()
+    }
+    
+    fileprivate func renderTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(PictureCell.self, forCellReuseIdentifier: cellId)
+        view.addSubview(tableView)
     }
 
-
+    fileprivate func loadImages() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath! // all resources will be bundled here, including our images
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasPrefix("nssl") {
+                self.pictures.append(item)
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! PictureCell
+        cell.textLabel?.text = pictures[indexPath.item]
+        cell.accessoryType = .disclosureIndicator
+        cell.tintColor = UIColor.blue
+        return cell
+    }
 }
 
